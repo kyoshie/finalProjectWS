db11e7b5ad4b6c2aa9542c142c699a50e06e6392
Kyoshie <157869727+kyoshie@users.noreply.github.com>
2024-07-22 01:24:14 +0000

    journal form creation

--- admin.php
+++ admin.php
@@ -4,17 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Dashboard</title>
-
-    <link rel= "stylesheet" href="admin.css">
+  <link rel= "stylesheet" href="admin.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <style>
-    
-  </style>
+
 </head>
 <body>
   <div class="wrapper d-flex">
-
     <!--sidebar start -->
     <aside id="sidebar" class="bg-dark">
       <div class="icon d-flex align-items-center p-3">
@@ -40,42 +36,53 @@
         </li>
       </ul>
 
-      
     </aside>
     <!--sidebar end -->
-
     <div id="page-content-wrapper">
         <nav class="navbar navbar-expand-lg py-4 px-4">
             <button id="toggle-btn" class="btn btn-secondary">
                 <i class="bi bi-grid"></i>
             </button>
         </nav>
+        <h1>Dashboard</h1>
 
-        <main id="content" class="p-4 flex-grow-1">
-            <h1>Dashboard</h1>
+        <main id="content" class="content p-4 flex-grow-1 mx-2">
             <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+              <h3>Journal</h3>
+                <div class="mb-3">
+                  <label class="form-label">Title</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1">
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Date</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                      <div class="mb-3">
+                        <label class="form-label">Issue</label>
+                        <input type="text" class="form-control" id="exampleInputIssue">
+                    </div>
+                  </div>
+                <div class="col-md-6">
+                  <div class="mb-3">
+                      <label class="form-label">Volume</label>
+                      <input type="text" class="form-control" id="exampleInputVolume">
+                  </div>
+                </div>
+            </div>
+            </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </main>
     </div>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
-    <script src="admin.js"></script>
+  <script src="admin.js"></script>
+  
 </body>
 </html>
 
