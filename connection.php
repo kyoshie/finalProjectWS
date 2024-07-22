6e406a0c107f07ce1bee044152819c417ae5a052
Kyoshie <157869727+kyoshie@users.noreply.github.com>
2024-07-22 00:00:26 +0000

    form

--- connection.php
+++ connection.php
@@ -1,14 +1,18 @@
 <?php 
     $host = 'localhost';
     $username = 'root';
-    $password='';
+    $password='secret';
     $dbname = 'journal';
 
 
-    $connection = mysqli_connect ($host, $username, $password, $dbname);
+    $connection = mysqli_connect($host, $username, $password, $dbname);
 
     if (!$connection) {
         die("Connection failed: ".mysqli_connect_error());
     }
+
+
+    echo "Connected";
+
 
 ?>
